8f9d77a0dcb1da560ba13f81b5e4984eee6b6465
michaelwehrley <michaelwehrley@gmail.com>
2014-10-21 11:20:37 -0500

    used rspec command to install missing files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 EnrollmentDashboard::Engine.routes.draw do
   root to: "participants#index"
-  resources :arms
   resources :enrollments
-  resources :groups
   resources :participants
-  resources :projects
-  resources :users
 end
--- lib/enrollment_dashboard/engine.rb
+++ lib/enrollment_dashboard/engine.rb
@@ -1,5 +1,9 @@
 module EnrollmentDashboard
   class Engine < ::Rails::Engine
     isolate_namespace EnrollmentDashboard
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
