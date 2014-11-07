85781d0706969dc365958a48d07df641bc2d1c05
michaelwehrley <michaelwehrley@gmail.com>
2014-11-07 15:03:32 -0600

    added a concern that sets the password for a user on create

---
+++ app/models/think_feel_do_dashboard/concerns/password.rb
@@ -0,0 +1,27 @@
+module ThinkFeelDoDashboard
+  module Concerns
+    # is included in the User model and automatically
+    # sets a password on create
+    module Password
+      extend ActiveSupport::Concern
+
+      # This is perfect for including functionality
+      # provided by 3rd party gems, etc.
+      included do
+        before_create :set_password
+      end
+
+      # methods added to Class itself...
+      module ClassMethods
+      end
+
+      private
+
+      def set_password
+        random_eight = SecureRandom.urlsafe_base64(nil, false).first(8)
+        self.password = random_eight
+        self.password_confirmation = random_eight
+      end
+    end
+  end
+end
--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -2,18 +2,10 @@
 # devise. These users have roles such as SuperAdmin,
 # Admin, Coach etc in the parent application
 class User < ActiveRecord::Base
+  include ThinkFeelDoDashboard::Concerns::Password
+
   has_many :coach_assignments, foreign_key: :coach_id, dependent: :destroy
   has_many :participants, through: :coach_assignments
   has_many :user_roles, dependent: :destroy
   validates :email, presence: true
-
-  before_create :set_password
-
-  private
-
-  def set_password
-    random_eight = SecureRandom.urlsafe_base64(nil, false).first(8)
-    self.password = random_eight
-    self.password_confirmation = random_eight
-  end
 end
