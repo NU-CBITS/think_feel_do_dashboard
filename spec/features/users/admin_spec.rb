58da51b6843e7becb2fa16f712a2efdd6a05aef7
michaelwehrley <michaelwehrley@gmail.com>
2014-11-07 11:32:44 -0600

    added in admin tests

---
+++ spec/features/users/admin_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+feature "Admin" do
+  fixtures :all
+
+  before do
+    visit "/think_feel_do_dashboard"
+  end
+
+  it "displays correct links" do
+    expect(page).to have_link "Users", href: "/think_feel_do_dashboard/users"
+    expect(page).to have_link "Projects", href: "/think_feel_do_dashboard/projects"
+    expect(page).to have_link "Arms", href: "/think_feel_do_dashboard/arms"
+    expect(page).to have_link "Groups", href: "/think_feel_do_dashboard/groups"
+    expect(page).to have_link "Participants", href: "/think_feel_do_dashboard/participants"
+  end
+end
