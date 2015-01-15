0a345cb89acc9abb6c5d85ff0a243f92c89f2a02
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-01-15 11:26:49 -0600

    version 0.11.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+### 0.11.1
+ * Report namespace bug fix
+
 ### 0.11.0
 
  * added reports
--- lib/think_feel_do_dashboard/version.rb
+++ lib/think_feel_do_dashboard/version.rb
@@ -1,4 +1,4 @@
 # Version number of engine
 module ThinkFeelDoDashboard
-  VERSION = "0.11.0"
+  VERSION = "0.11.1"
 end
