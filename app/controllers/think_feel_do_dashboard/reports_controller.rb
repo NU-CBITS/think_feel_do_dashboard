ba951b9573287db05f4a37638458d55d7aa1c7ad
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-02-24 07:47:35 -0600

    remove all reports and refactor reports controller

--- app/controllers/think_feel_do_dashboard/reports_controller.rb
+++ app/controllers/think_feel_do_dashboard/reports_controller.rb
@@ -3,64 +3,29 @@ require_dependency "think_feel_do_dashboard/application_controller"
 module ThinkFeelDoDashboard
   # Provide access to data exports.
   class ReportsController < ApplicationController
-    REPORTS = [
-      {
-        title: "Site Session",
-        id: :site_session,
-        klass: Reports::SiteSession
-      },
-      {
-        title: "Task Completion",
-        id: :task_completion,
-        klass: Reports::TaskCompletion
-      },
-      {
-        title: "Tool Access",
-        id: :tool_access,
-        klass: Reports::ToolAccess
-      },
-      {
-        title: "User Agent",
-        id: :user_agent,
-        klass: Reports::UserAgent
-      },
-      {
-        title: "Login",
-        id: :login,
-        klass: Reports::Login
-      },
-      {
-        title: "PHQ9 Assessment",
-        id: :phq_assessment,
-        klass: Reports::PhqAssessment
-      },
-      {
-        title: "WAI Assessment",
-        id: :wai_assessment,
-        klass: Reports::WaiAssessment
-      }
-    ]
-
     def index
-      authorize! :read, Reports::SiteSession
-      reports
+      authorize! :read, "Reports"
+
+      fetch_reports
     end
 
     def show
-      report = (reports.find { |r| r[:id].to_s == params[:id] } || {})[:klass]
+      authorize! :read, "Reports"
 
-      authorize! :download, report
+      report = fetch_reports.find { |r| r[:id].to_s == params[:id] } || {}
+      report = report[:klass]
+
       respond_to { |fmt| fmt.csv { send_data report.to_csv, type: "text/csv" } }
     end
 
     private
 
-    def reports
-      @reports = REPORTS
+    def fetch_reports
+      @reports = []
 
       return @reports unless Rails.application.config.respond_to?(:reports)
 
-      @reports += Rails.application.config.reports
+      @reports = Rails.application.config.reports
     end
   end
 end
