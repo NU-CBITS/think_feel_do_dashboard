a30e0efb93c06d638379d49b91f2e5428fff9646
Eric Schlange <eric.schlange@gmail.com>
2015-04-02 10:49:14 -0500

    Fix rubocop.
    * Fixed rubocop issue with method call format.
    
    [#91623394]

--- app/helpers/think_feel_do_dashboard/memberships_helper.rb
+++ app/helpers/think_feel_do_dashboard/memberships_helper.rb
@@ -4,7 +4,7 @@ module ThinkFeelDoDashboard
     def projected_end_date(membership)
       offset = Rails.application.config.study_length_in_weeks * 7
       start_day = membership.start_date ? membership.start_date : Date.today
-      I18n.l (start_day + offset.days), format: :standard
+      I18n.l((start_day + offset.days), format: :standard)
     end
   end
 end
