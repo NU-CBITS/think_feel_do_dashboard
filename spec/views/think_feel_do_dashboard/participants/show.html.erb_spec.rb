3c01db6275034f181be266189013229f0700a97f
michaelwehrley <michaelwehrley@gmail.com>
2015-12-16 11:03:29 -0600

    Improve Flash Message
    * Augment flash message for participant deletion
    * Add view spec
    
    [Finished: #110256638]

---
+++ spec/views/think_feel_do_dashboard/participants/show.html.erb_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "think_feel_do_dashboard/participants/show.html.erb",
+               type: :view do
+  fixtures :participants
+
+  context "when viewing the participant" do
+    let(:participant) { participants(:participant1) }
+
+    it "has a comprehensive deletion dialog" do
+      assign :participant, participant
+
+      allow(view).to receive(:can?).and_return(true)
+      allow(view).to receive(:group_path).and_return("#")
+      allow(view).to receive(:edit_participant_path).and_return("#")
+      allow(view).to receive(:participant_path).and_return("#")
+      allow(view).to receive(:social_features?)
+      allow(view).to receive(:participant_group_path).and_return("#")
+      allow(view).to receive(:new_participant_group_path).and_return("#")
+      allow(view).to receive(:participant_coach_path).and_return("#")
+      allow(view).to receive(:woz_participant_coach_path).and_return("#")
+
+      render
+
+      expect(rendered)
+        .to include "Are you sure you want to delete this participant? "\
+        "All participant data will be deleted along with other "\
+        "participant data associated with this data. "\
+        "This action cannot be undone."
+    end
+  end
+end
